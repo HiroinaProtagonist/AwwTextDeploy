ae1607a04a6605c1d1eff9a33efe7bc3a296111b
Zoe Abrams <zoej.abrams@gmail.com>
2020-10-26 00:07:31 -0700

    Remove error message update after handling error

--- client/src/MMSRow.js
+++ client/src/MMSRow.js
@@ -14,8 +14,8 @@ const MMSRow = () => (
             </div>
             <div id="aboutText">
                 Send /r/aww's top picture of the day to your chosen number.<br/><br/>
-                <b>Status update:</b> The app has recently been experiencing failure to send messages.<br/>
-                This has been resolved and it should now function as expected.
+                {/* <b>Status update:</b> The app has recently been experiencing failure to send messages.<br/>
+                This has been resolved and it should now function as expected. */}
             </div>
         </div>
     </>
